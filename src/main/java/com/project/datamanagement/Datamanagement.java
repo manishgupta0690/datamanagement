f0aeb5e71b5e7815787f07dd407e98d7d644c2e9
manish <manishgupta0690@gmail.com>
2024-04-16 22:03:36 +0100

    project structure

---
+++ src/main/java/com/project/datamanagement/Datamanagement.java
@@ -0,0 +1,12 @@
+package com.project.datamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Datamanagement {
+	public static void main(String[] args) {
+		SpringApplication.run(Datamanagement.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/datamanagement/DatamanagementTest.java
@@ -0,0 +1,14 @@
+package com.project.datamanagement;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DatamanagementTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
